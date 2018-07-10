bac9ea5c94718842050d9e43372d281be8e906c2
Loic El Thesea <1372314+loicelthesea@users.noreply.github.com>
2018-07-10 16:33:29 +0200

    Add firebase config file (removed from gitignore)

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+
+export default {
+  apiKey: "AIzaSyCZlfj-pzom0aVtmruviFEtvGPRsnPcxGA",
+  authDomain: "expensify-148fb.firebaseapp.com",
+  databaseURL: "https://expensify-148fb.firebaseio.com",
+  projectId: "expensify-148fb",
+  storageBucket: "expensify-148fb.appspot.com",
+  messagingSenderId: "663089694482"
+};
